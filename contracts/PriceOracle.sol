fd9390741484902f3403668e2dc464fbd38d14b8
Ben J Kanbour <bjkanbour@gmail.com>
2022-05-26 21:41:38 -0500

    fake oracle

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -5,12 +5,15 @@ import "./CToken.sol";
 contract PriceOracle {
     /// @notice Indicator that this is a PriceOracle contract (for inspection)
     bool public constant isPriceOracle = true;
+    uint value;
 
     /**
       * @notice Update the price of an underlying asset
       * @param cToken The cToken to update the underlying price of
       */
-    function updatePrice(CToken cToken) external;
+    function updatePrice(CToken cToken) external{
+      getUnderlyingPrice(cToken);
+    }
 
     /**
       * @notice Get the underlying price of a cToken asset
@@ -18,5 +21,11 @@ contract PriceOracle {
       * @return The underlying asset price mantissa (scaled by 1e18).
       *  Zero means the price is unavailable.
       */
-    function getUnderlyingPrice(CToken cToken) external view returns (uint);
+    function getUnderlyingPrice(CToken cToken) external view returns (uint){
+      return (value);
+    }
+
+    function setValue(uint _value) external{
+      value = _value;
+    }
 }
