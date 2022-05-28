39ca460323e16e700a22ed89ac1adf34c8c777eb
John Mosesman <johnmosesman@gmail.com>
2022-05-28 13:52:17 -0500

    MockPriceOracle

--- contracts/MockPriceOracle.sol
+++ contracts/MockPriceOracle.sol
@@ -1,18 +1,18 @@
 pragma solidity ^0.5.16;
 
-import "./CToken.sol";
-
-contract PriceOracle {
+contract MockPriceOracle {
     /// @notice Indicator that this is a PriceOracle contract (for inspection)
     bool public constant isPriceOracle = true;
     uint value;
+
+    mapping(address => uint) public prices;
 
     /**
       * @notice Update the price of an underlying asset
       * @param cToken The cToken to update the underlying price of
       */
-    function updatePrice(CToken cToken) external{
-      // getUnderlyingPrice(cToken);
+    function updatePrice(address cToken, uint price) external{
+      prices[cToken] = price;
     }
 
     /**
@@ -21,11 +21,7 @@ contract PriceOracle {
       * @return The underlying asset price mantissa (scaled by 1e18).
       *  Zero means the price is unavailable.
       */
-    function getUnderlyingPrice(CToken cToken) external view returns (uint){
-      return (value);
-    }
-
-    function setValue(uint _value) external{
-      value = _value;
+    function getUnderlyingPrice(address cToken) external view returns (uint){
+      return prices[cToken];
     }
 }
---
+++ test/MockPriceOracle.js
@@ -0,0 +1,38 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MockPriceOracle contract", function () {
+  describe("updatePrice()", async () => {
+    it("sets the price", async () => {
+      const MockFactory = await ethers.getContractFactory("MockPriceOracle");
+      const contract = await MockFactory.deploy();
+
+      const cTokenAddress = ethers.Wallet.createRandom().address;
+      await contract.updatePrice(cTokenAddress, 10);
+
+      expect(await contract.prices(cTokenAddress)).to.eq(10);
+    });
+  });
+
+  describe("getUnderlyingPrice()", async () => {
+    it("returns 0 if the price hasn't been set", async () => {
+      const MockFactory = await ethers.getContractFactory("MockPriceOracle");
+      const contract = await MockFactory.deploy();
+
+      const cTokenAddress = ethers.Wallet.createRandom().address;
+      // await contract.updatePrice(cTokenAddress, 10);
+
+      expect(await contract.getUnderlyingPrice(cTokenAddress)).to.eq(0);
+    });
+
+    it("returns the price if the price has been set", async () => {
+      const MockFactory = await ethers.getContractFactory("MockPriceOracle");
+      const contract = await MockFactory.deploy();
+
+      const cTokenAddress = ethers.Wallet.createRandom().address;
+      await contract.updatePrice(cTokenAddress, 10);
+
+      expect(await contract.getUnderlyingPrice(cTokenAddress)).to.eq(10);
+    });
+  });
+});
