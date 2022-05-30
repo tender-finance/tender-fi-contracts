0f2c066631192ea1d52848c8a5896ceddb3d702e
John Mosesman <johnmosesman@gmail.com>
2022-05-30 11:51:37 -0500

    Loop over ctokens to get multiple assets to test against with same underlying

--- scripts/deploy-ctoken.ts
+++ scripts/deploy-ctoken.ts
@@ -8,25 +8,46 @@ const outputFilePath = `./deployments/${hre.network.name}.json`;
 const CTOKEN_DECIMALS = 8;
 
 // CToken Params
-const params = {
-  underlying: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
-  // comptroller: "0xAb5e6011B22aC9bC3D5630F0e7Fbc6C1fEC44EfE",
-  // irModel: "0xE2dEeD50F4Ce1758042C732B9D5dEDbd49b340d2",
-  name: "tMetis",
-  symbol: "tMetis",
-  decimals: CTOKEN_DECIMALS,
-};
+// const params = {
+//   underlying: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+//   name: "tMetis",
+//   symbol: "tMetis",
+//   decimals: CTOKEN_DECIMALS,
+// };
 
 export async function main() {
+  const d = [
+    {
+      underlying: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+      name: "tMetis",
+      symbol: "tMetis",
+      decimals: CTOKEN_DECIMALS,
+    },
+    {
+      underlying: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+      name: "bMetis",
+      symbol: "bMetis",
+      decimals: CTOKEN_DECIMALS,
+    },
+    {
+      underlying: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+      name: "kMetis",
+      symbol: "kMetis",
+      decimals: CTOKEN_DECIMALS,
+    },
+  ];
+
+  let p = d[0];
+
   const [deployer] = await hre.ethers.getSigners();
   console.log(`>>>>>>>>>>>> Deployer: ${deployer.address} <<<<<<<<<<<<\n`);
 
   const erc20Underlying = await hre.ethers.getContractAt(
     "EIP20Interface",
-    params.underlying
+    p.underlying
   );
   const underlyingDecimals = await erc20Underlying.decimals();
-  const totalDecimals = underlyingDecimals + params.decimals;
+  const totalDecimals = underlyingDecimals + p.decimals;
   const initialExcRateMantissaStr = numToWei("2", totalDecimals);
 
   const CErc20Immutable = await hre.ethers.getContractFactory(
@@ -40,50 +61,54 @@ export async function main() {
   const irModelAddress: string =
     deployments.IRModels.WhitePaperInterestRateModel["6000000"]["0__1500"];
 
-  const cErc20Immutable = await CErc20Immutable.deploy(
-    params.underlying,
-    unitrollerAddress,
-    irModelAddress,
-    initialExcRateMantissaStr,
-    params.name,
-    params.symbol,
-    params.decimals,
-    deployer.address
-  );
-  await cErc20Immutable.deployed();
-  console.log("CErc20Immutable deployed to:", cErc20Immutable.address);
-
-  const unitrollerProxy = await hre.ethers.getContractAt(
-    "Comptroller",
-    unitrollerAddress
-  );
-
-  console.log("calling unitrollerProxy._supportMarket()");
-
-  await unitrollerProxy._supportMarket(cErc20Immutable.address);
-
-  let confirmations = hre.network.name === "metis" ? 15 : 1;
-  await cErc20Immutable.deployTransaction.wait(confirmations);
-
-  // Save to output
-  deployments[params.symbol] = cErc20Immutable.address;
-  writeFileSync(outputFilePath, JSON.stringify(deployments, null, 2));
-
-  try {
-    await verifyContract(cErc20Immutable.address, [
-      params.underlying,
+  let tokens = d.forEach(async (token) => {
+    const cErc20Immutable = await CErc20Immutable.deploy(
+      token.underlying,
       unitrollerAddress,
       irModelAddress,
       initialExcRateMantissaStr,
-      params.name,
-      params.symbol,
-      params.decimals,
-      deployer.address,
-    ]);
-  } catch (e) {
-    console.error("Error verifying cErc20Immutable", cErc20Immutable.address);
-    console.error(e);
-  }
+      token.name,
+      token.symbol,
+      token.decimals,
+      deployer.address
+    );
+    await cErc20Immutable.deployed();
+    console.log("CErc20Immutable deployed to:", cErc20Immutable.address);
+
+    const unitrollerProxy = await hre.ethers.getContractAt(
+      "Comptroller",
+      unitrollerAddress
+    );
+
+    console.log("calling unitrollerProxy._supportMarket()");
+
+    await unitrollerProxy._supportMarket(cErc20Immutable.address);
+
+    let confirmations = hre.network.name === "metis" ? 15 : 1;
+    await cErc20Immutable.deployTransaction.wait(confirmations);
+
+    // Save to output
+    deployments[token.symbol] = cErc20Immutable.address;
+    writeFileSync(outputFilePath, JSON.stringify(deployments, null, 2));
+
+    try {
+      await verifyContract(cErc20Immutable.address, [
+        token.underlying,
+        unitrollerAddress,
+        irModelAddress,
+        initialExcRateMantissaStr,
+        token.name,
+        token.symbol,
+        token.decimals,
+        deployer.address,
+      ]);
+    } catch (e) {
+      console.error("Error verifying cErc20Immutable", cErc20Immutable.address);
+      console.error(e);
+    }
+  });
+
+  // await Promise.all(tokens);
 }
 
 const verifyContract = async (
--- scripts/update-jumprate-model.ts
+++ scripts/update-jumprate-model.ts
@@ -10,42 +10,54 @@ const params = {
   kink: "80",
   multiplierPreKink: "20",
   multiplierPostKink: "100",
-}
+};
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log(`>>>>>>>>>>>> Account: ${deployer.address} <<<<<<<<<<<<\n`);
 
-  const jumpMultiplier = getJumpMultiplier(params.kink, params.multiplierPreKink, params.multiplierPostKink);
+  const jumpMultiplier = getJumpMultiplier(
+    params.kink,
+    params.multiplierPreKink,
+    params.multiplierPostKink
+  );
 
   const baseRateWei = numToWei(toBn(params.baseRate).div(100), 18);
   const kinkWei = numToWei(toBn(params.kink).div(100), 18);
   const multiplierWei = numToWei(toBn(params.multiplierPreKink).div(100), 18);
   const jumpMultiplierWei = numToWei(toBn(jumpMultiplier).div(100), 18);
 
-  const jumpRateModelV2 = await hre.ethers.getContractAt("JumpRateModelV2", params.address);
+  const jumpRateModelV2 = await hre.ethers.getContractAt(
+    "JumpRateModelV2",
+    params.address
+  );
   const tx = await jumpRateModelV2.updateJumpRateModel(
     params.blocksPerYear,
     baseRateWei,
     multiplierWei,
     jumpMultiplierWei,
-    kinkWei,
+    kinkWei
   );
   await tx.wait();
 
   console.log(`JumpRateModelV2 updated in txn: ${tx.hash}.`);
 }
 
-const getJumpMultiplier = (kink: string, multiplierPreKink: string, multiplierPostKink: string): string => {
+const getJumpMultiplier = (
+  kink: string,
+  multiplierPreKink: string,
+  multiplierPostKink: string
+): string => {
   return toBn(multiplierPostKink)
     .minus(multiplierPreKink)
     .div(toBn(100).minus(kink))
-    .times(100).toFixed();
-}
+    .times(100)
+    .toFixed();
+};
 
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+// main()
+//   .then(() => process.exit(0))
+//   .catch(error => {
+//     console.error(error);
+//     process.exit(1);
+//   });
